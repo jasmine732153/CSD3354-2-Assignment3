e4d7f6c7d94671eb67a5208f5bf5b390c95ae609
visualstudio <visualstudio@122-PC17>
2019-03-13 11:21:57 -0400

    final commit

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -2,46 +2,82 @@
 using BankAccounts;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace BankTests
-
+namespace UnitTestProject1
 {
     [TestClass]
-    public class BankAccountTest
-    { 
-        public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
-        public const string DebitAmounlessThanZeroMessage = "Debit amount is less than zero";
-
-        
+    public class BankTests
     {
         [TestMethod]
-
-
-        public void Debit_withValidAmount_UpdatesBalance()
+        public void Debit_Withvalidamount_UpdatesBalance()
         {
             //Arrange
-            double begginingBalance = 11.99;
+            double beginningBalance = 11.99;
             double debitAmount = 4.55;
             double expected = 7.44;
-            BankAccount account = new BankAccount("Mr. Bryan Walton", begginingBalance);
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            //Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.01, "Account not debited correctly");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
 
             //Act
             account.Debit(debitAmount);
-            //Assert
-            double actual = account.Balance;
-            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+
+            //Assert is handled by the ExpectedException  attribute on the test method
+
+
+
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 20.0;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            //Act
+            try
+            {
+                account.Debit(debitAmount);
+            }
+
+            catch (ArgumentOutOfRangeException e)
+            {
+                //Assert
+                StringAssert.Contains(e.Message, BankAccount.DebitamountExceedsBalanceMessage);
+            }
+
+
         }
 
-    }
-    [TestMethod]
-    [ExpectedException(typeof(ArgumentException))]
-    public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
-    {
-        //arrange
-        double beginingBalance = 11.99;
-        double debitAmount = -100.00;
-        BankAccount account = new BankAccount("Mr.Bryan Walton", beginingBalance);
-        // act
-        account.Debit(debitAmount);
-        // Asert is handled by the ExpectedExpection attribute on the test method.
+
+
+
+
     }
 }
+
+
+
+
+
+
+
+
+
