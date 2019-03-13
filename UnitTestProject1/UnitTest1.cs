892a52213b6bd3aa6e6837220ddf8e7a140bcf08
visualstudio <visualstudio@122-PC17>
2019-03-13 10:59:51 -0400

    10.59 commit

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -6,12 +6,34 @@ namespace BankTests
 
 {
     [TestClass]
-    public class BankAccountTests
+    public class BankAccountTest
     {
         [TestMethod]
-        public void TestMethod1()
-        {
 
+
+        public void Debit_withValidAmount_UpdatesBalance()
+        {
+            //Arrange
+            double begginingBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", begginingBalance);
+
+            //Act
+            account.Debit(debitAmount);
+            //Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
         }
+
+    }
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+    {
+        //arrange
+        double beginingBalance = 11.99;
+        double debitAmount = -100.00;
+        BankAccount account = new BankAccount("Mr.Bryan Walton", beginingBalance);
     }
 }
