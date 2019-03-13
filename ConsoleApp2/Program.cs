ea04ce2b29179b0b35776ee1501fd2827b380f40
visualstudio <visualstudio@122-PC17>
2019-03-13 08:54:51 -0400

    last commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -70,16 +70,12 @@ namespace BankAccounts
         }
         public static void Main()
         {
-            BankAccount ba = newBankAccount("Mr.BRYan Walton", 11.99);
+            BankAccount ba = new BankAccount("Mr.BRYan Walton", 11.99);
             ba.Credit(5.77);
             ba.Debit(11.22);
             Console.WriteLine("Current balance is ${0}", ba.Balance);
+            Console.ReadLine();
         }
     }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+    
 }
