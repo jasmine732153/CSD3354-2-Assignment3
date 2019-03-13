db7bb72b569b9f4093d975a11113d3a7779e27c7
visualstudio <visualstudio@122-PC17>
2019-03-13 10:06:55 -0400

    10 am commit

--- UnitTestProject2/UnitTest1.cs
+++ UnitTestProject2/UnitTest1.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace UnitTestProject1
+namespace UnitTestProject2
 {
     [TestClass]
     public class UnitTest1
@@ -9,7 +9,6 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
-
         }
     }
 }
