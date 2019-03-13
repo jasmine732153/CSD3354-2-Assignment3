ce698870e718d332b65efd4821a076912adddb5b
visualstudio <visualstudio@122-PC17>
2019-03-13 08:46:25 -0400

    final commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -60,6 +60,21 @@ namespace BankAccounts
             }
             m_balance += amount;
         }
+        private void FreezeAccount ()
+        {
+            m_frozen = true;
+        }
+        private void UnfreezeAccount()
+        {
+            m_frozen = false;
+        }
+        public static void Main()
+        {
+            BankAccount ba = new.BankAccount("Mr.BRYan Walton", 11.99);
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+        }
     }
     class Program
     {
