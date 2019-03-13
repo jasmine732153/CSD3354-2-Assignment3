de3124b12542a258de1668a78635777050f9694a
visualstudio <visualstudio@122-PC17>
2019-03-13 08:40:53 -0400

    second commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -14,6 +14,52 @@ namespace BankAccounts
         private string m_customerName;
         private double m_balance;
         private bool m_frozen = false;
+        private BankAccount()
+        {
+
+        }
+        public BankAccount(String customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+        public void Debit(double amount)
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Account Frozen");
+            }
+            if (amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+
+            }
+            if (amount<0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            m_balance += amount;//intentionally incorrect code
+        }
+        public void Credit(double amount)
+        {
+            if(m_frozen)
+            {
+                throw new ArgumentOutOfRangeException("Account frozen");
+            }
+            if (amount<0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            m_balance += amount;
+        }
     }
     class Program
     {
